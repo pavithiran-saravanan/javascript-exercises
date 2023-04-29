56c4cf048ac0796eb7f22828a57f12e8cd1c54f8
pavithiran <pavithransaravanan22@gmail.com>
2023-04-29 20:33:50 +0530

    Complete exercies 5 to 7

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    // Return error for non numeric values and negative numbers
+    if(typeof 1 != typeof num1 || typeof 1 != typeof num2 || num1 < 0 || num2 < 0) return 'ERROR';
+    // Swap num1 and num2 if num1 is greater
+    if(num1>num2) [num1, num2] = [num2, num1];
+    let sum = 0;
+    for(let i = num1; i <= num2; i++){
+        sum = sum + i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp_f) {
+  return Math.round((temp_f - 32) * (5/9) * 10)/10; 
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp_c) {
+  return Math.round((temp_c*(9/5) + 32) * 10)/10;
 };
 
 // Do not edit below this line
@@ -9,3 +11,11 @@ module.exports = {
   convertToCelsius,
   convertToFahrenheit
 };
+
+/*
+
+Farenheight to Celsius
+(x − 32) * (5/9)
+ 
+
+*/
