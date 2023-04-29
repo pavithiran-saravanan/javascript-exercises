9a30406d095ae02e82c42f0e7d33eb3c1bc6e743
pavithiran <pavithransaravanan22@gmail.com>
2023-04-29 17:09:53 +0530

    Complete exercises 3 and 4

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,15 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    revStr = '';
+    for(let i = str.length; i >= 0; i--){
+        revStr = revStr + str.charAt(i);
+    }
+    return revStr;
 };
+
+/*
+Strings in javascript cannot be reversed as it is.
+Strings have to be split into something, next reversed and form strings again
+*/
 
 // Do not edit below this line
 module.exports = reverseString;
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,25 @@
-const removeFromArray = function() {
+// Solution-1
+// const removeFromArray = function(arr) {
+//     // Iterate over each of the arguments
+//     for(let i = 1; i < arguments.length; i++){
+//         let index = arr.indexOf(arguments[i]);
+//         if(index != -1){
+//             arr.splice(index, 1);
+//         }
+//     }
+//     return arr;
+// };
 
+// Solution-2
+const removeFromArray = function(arr, ...args) {
+    // Iterate over each of the arguments
+    for(let arg of args){
+        let index = arr.indexOf(arg);
+        if(index != -1){
+            arr.splice(index, 1);
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
