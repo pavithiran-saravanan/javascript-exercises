950009719b7962fa1ec2d101f300ec921f74b963
pavithiran <pavithransaravanan22@gmail.com>
2023-04-29 16:03:10 +0530

    Complete exercises 1 and 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(word, count) {
+    if(count < 0) return 'ERROR';
+    let repeatedWord = "";
+    for(let i = 0; i < count; i++){
+        repeatedWord = repeatedWord + word;
+    }
+    return repeatedWord;
 };
 
 // Do not edit below this line
