8b1788054ac61b3b275a3dac3b2dbc5821570a5d
pavithiran <pavithransaravanan22@gmail.com>
2023-05-04 21:15:49 +0530

    Complete exercise 12 findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
+function getAge(a){
+    if(!a.yearOfDeath){
+        return (new Date()).getFullYear() - a.yearOfBirth;        
+    }
+    else{
+        return a.yearOfDeath - a.yearOfBirth;
+    }
+}
 
+const findTheOldest = function(people) {
+    return people.sort((a, b)=>{
+        const a_age = getAge(a);
+        const b_age = getAge(b);
+        return a_age < b_age ? 1 : -1;
+    })[0];
 };
 
 // Do not edit below this line
