a0192023f22bd50746dd3172092be639043b9464
pavithiran <pavithransaravanan22@gmail.com>
2023-05-04 20:43:07 +0530

    Complete exercise 10 fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    // We have to return the nth fib number
+    n = +n;
+    if(n < 0) return 'OOPS';
+    if(n <= 2) return 1;
+    let first = 1, second = 1, third = 0;
+    for(let i = 3; i <= n; i++){
+        third = first + second;
+        first = second;
+        second = third;
+    }
+    return third;
 };
 
 // Do not edit below this line
