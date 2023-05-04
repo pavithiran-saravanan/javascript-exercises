d88b97e56e48d84a187e4175089faf099e511472
pavithiran <pavithransaravanan22@gmail.com>
2023-05-04 20:02:32 +0530

    Complete exercise 8 calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(nums) {
+  return nums.reduce((sum, n)=>sum+n, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(nums) {
+  return nums.reduce((sum, n)=>sum*n, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	let power = 1;
+  for(let i = 0; i < b; i++){
+    power = power*a;
+  }
+  return power;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	let fact = 1;
+  for(let i = n; i > 0; i--){
+    fact = fact * i;
+  }
+  return fact;
 };
 
 // Do not edit below this line
