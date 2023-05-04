335336a8f290b0f4ac2d35335a7d7f9519189c48
pavithiran <pavithransaravanan22@gmail.com>
2023-05-04 20:18:28 +0530

    Complete exercise 9 palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    res = "";
+    Array.from(str).forEach((c)=>{
+        if(c != ' ' && c != '.' && c != ',' && c != '!') res = res + c;
+    })
+    res = res.toLowerCase();
 
+    reverse = res.split('').reverse().join('');
+    return res == reverse;
 };
 
 // Do not edit below this line
